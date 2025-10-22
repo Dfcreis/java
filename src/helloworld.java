fbdb0aec77d88d5c2b0b089923ff2519611ff1bf
Daniel <danieldfcreis@gmail.com>
2025-10-22 15:17:52 -0300

    aprendendo java

---
+++ src/helloworld.java
@@ -0,0 +1,16 @@
+public class helloworld {
+    public static void main(String [] args){
+        System.out.print("hello, world");
+    //8 Bits, intervalo de -128 a 127
+    byte oitoBits = 100;
+    //16 Bits, intervalo de -32,768 a 32,767
+    short numeroMuitoPequeno = 32;
+    //32 Bits, intervalo de -2^31 a 2^31-1
+    int inteiro = 32000;
+    // 64 Bits, Intervalo de -2^63 a 2^63-1
+    long numeroMuitoGrande = 164000;
+    // 32 bits, ponto flutuante de precisao simples
+    float decimalSimples = 31;
+
+    }
+}
